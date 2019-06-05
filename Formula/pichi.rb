8ac367d7b8c912d9fa7feccac7e20c42b6234e2a
Pichi <pichi@elude.in>
2019-06-05 17:20:26 +1100

    1.2.1-rc

--- Formula/pichi.rb
+++ Formula/pichi.rb
@@ -1,8 +1,8 @@
 class Pichi < Formula
   desc "Application Layer Proxy controlled via RESTful APIs"
   homepage "https://github.com/pichi-router/pichi"
-  url "https://github.com/pichi-router/pichi/archive/1.2.0.tar.gz"
-  sha256 "8965077371f2dbf49a63659d686470c191c2c21c47a32f44b3b8bb2d72968043"
+  url "https://github.com/pichi-router/pichi/archive/1.2.1-rc.tar.gz"
+  sha256 "f02740c3dac6475164ef20e010f4df32043f06e4682f9f6032753e24c241e950"
   depends_on "cmake" => :build
   depends_on "rapidjson" => :build
   depends_on "boost"
@@ -14,17 +14,16 @@ class Pichi < Formula
   def install
     cmake_args = *std_cmake_args
     cmake_args.delete_if { |opt| opt.start_with?("-DCMAKE_BUILD_TYPE") }
-    cmake_args << "-DVERSION=1.2.0"
+    cmake_args << "-DVERSION=1.2.1-rc"
     cmake_args << "-DCMAKE_BUILD_TYPE=MinSizeRel"
-    cmake_args << "-DBUILD_TEST=ON"
+    cmake_args << "-DBUILD_TEST=OFF"
     cmake_args << "-DSTATIC_LINK=OFF"
     cmake_args << "-DENABLE_TLS=ON"
+    cmake_args << "-DINSTALL_HEADERS=ON"
     cmake_args << "-DOPENSSL_ROOT_DIR=" + Formula["openssl"].opt_prefix
     system "cmake", *cmake_args, "."
     system "cmake", "--build", buildpath.to_s, ENV.deparallelize
-    system "cmake", "--build", buildpath.to_s, "--target", "test"
     system "cmake", "--build", buildpath.to_s, "--target", "install/strip"
-    include.install "include/pichi.h"
     etc.install "server/pichi.json.default"
     libexec.install "test/geo.mmdb"
     (libexec/"sbin/run_pichi").write <<~EOS
